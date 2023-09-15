e6d22c2fff0e3e0dea3a6d91791599d124f22755
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-09-15 19:21:31 +0530

    first commit

---
+++ NunitTestProject/NunitTestProject/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace NunitTestProject
+{
+    
+    public class Program
+    {
+        [Test]
+            public void TestAddition()
+            {
+                int num1 = 5; int num2 = 3;
+                int result = num1 + num2;
+                Assert.AreEqual(8, result);
+            }
+            [Test]
+            public void TestMultiplication() { int num1 = 5; int num2 = 3; int result = num1 * num2; Assert.AreEqual(15, result); }
+            [Test]
+            public void TestSubtaction() { int num1 = 5; int num2 = 2; int result = num1 - num2; Assert.AreEqual(3, result); }
+        
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
